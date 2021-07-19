a8517487ea0f494a8c8aa74ea8ea532e34d43558
Catastrophie <u6004654@utah.edu>
2021-07-19 17:35:00 -0600

    Everything is storing successfully. Need to pull the final text document from a linux system. It doesn't like finding the correct path via windows VS ssh debug

--- JSONStaging.cpp
+++ JSONStaging.cpp
@@ -1,123 +1,78 @@
-#include <iostream>  
-#include <fstream> 
+#include <iostream>
+#include <fstream>
 #include <vector>
+#include <sstream>
 #include "JSONStaging.h"
 
 
 //Main
-int main2()
+int main()
 {
+    std::string holdString;
     std::ifstream timeStampMonitorStats("testingTempdata.txt");
+    std::stringstream buffer;
+    buffer << timeStampMonitorStats.rdbuf();
+    holdString = buffer.str();
+    timeStampMonitorStats.close();
 
-    while (true)
-    {
-        timeStampMonitorStats >> holdString;
-       // if (timeStampMonitorStats.fail()) {
-       //     timeStampMonitorStats.close();
-            //Create listening loop for change to size or sleep
-       //     break; //may not need exactly like this
-       //}
+    std::string opening = "defaultdict(<class 'dict'>, {";
 
-        //endOfFileRead = timeStampMonitorStats.tellp();
+    holdString.erase(0, opening.length());
+    holdString.erase(holdString.find("})"), holdString.length());
 
-        holdString.erase(0, holdString.find("defaultdict(<class 'dict'>, {"));
-        holdString.erase(holdString.find("})"), holdString.length());
+    //endOfFileRead = timeStampMonitorStats.tellp();
 
-        //contain timestamp of readings
-        std::string timestamp = holdString.substr(0, holdString.find(": {'"));
-        holdString.erase(0, timestamp.length() + 5);
-        timestampVect.push_back(timestamp);
+    while (holdString.length() > 0)
+        holdString = constructData(holdString);
 
-        //loop through each system and create vector for the current timestamp
-        for (int i = 0; i < systemCount; i++) {
-            std::pair<std::string, std::string> sysValue;
-            std::string compName = holdString.substr(0, holdString.find("': '"));
-            holdString.erase(0, compName.length() + 4);
-            sysValue.first = compName;
+    createFile();
+
+    return 0;
+}
+
+void createFile() {
+
+    int last = timestampVect.size() - 1;
+    // Open the file for Temperature data and save the most recent stats
+    //      NEEDS ADJUSTED TO RETRIEVE THE LAST DATA VALUES???
+    std::ofstream dataFile;
+    dataFile.open("unrealStageTemp.txt");
+    dataFile << timestampVect.at(last) << "\n";
+
+    //
+    if (last > 0) {
+        for (int i = (last * 5) - 1; i < ((last + 1) * 5) - 1; i++) { //May break
+            dataFile << statBySystem.at(i).first << " : ";
+            dataFile << statBySystem.at(i).second << "\n";
+        }
+        dataFile.close();
+    }
+}
+
+std::string constructData(std::string holdString) {
+    //contain timestamp of readings
+    std::string timestamp = holdString.substr(0, holdString.find(": {'"));
+    holdString.erase(0, timestamp.length() + 4);
+    timestampVect.push_back(timestamp);
+
+    //loop through each system and create vector for the current timestamp
+    for (int i = 0; i < systemCount; i++) {
+        std::pair<std::string, std::string> sysValue;
+        std::string compName = holdString.substr(0, holdString.find("': '"));
+        holdString.erase(0, compName.length() + 4);
+        sysValue.first = compName;
+
+        if (holdString.find("', '") > holdString.find("'}, ")) {
+            std::string currentValue = holdString.substr(0, holdString.find("'}, "));
+            holdString.erase(0, currentValue.length() + 4);
+            sysValue.second = currentValue;
+        }
+        else {
             std::string currentValue = holdString.substr(0, holdString.find("', '"));
             holdString.erase(0, currentValue.length() + 4);
             sysValue.second = currentValue;
-
-            statBySystem.push_back(sysValue);
         }
-
-        int last = timestampVect.size();
-        // Open the file for Temperature data and save the most recent stats 
-        //      NEEDS ADJUSTED TO RETRIEVE THE LAST DATA VALUES???
-        std::ofstream dataFile;
-        dataFile.open("unrealStageTemp.txt");
-        dataFile << timestampVect.at(last) << "\n";
-
-        //
-        if (last > 0) {
-            for (int i = ((last * 5) + 1) - 5; i < ((last * 5) + 1); i++) { //May break
-                dataFile << statBySystem.at(i).first << " : ";
-                dataFile << statBySystem.at(i).second << "\n";
-            }
-            dataFile.close();
-        }
+        statBySystem.push_back(sysValue);
     }
+    return holdString;
 }
-/*
-defaultdict(<class 'dict'>, {
-    1623427831
-    : {'
-    ddc-infra-pdu-h9-1.redhorn
-    ': '
-    44
-    ', '
-    ddc-infra-pdu-q5-1.redhorn
-    ': '
-    43
-    ', '
-    ddc-infra-pdu-s11-1.redhorn
-    ': '
-    39
-    ', '
-    ddc-infra-pdu-u11-1.redhorn
-    ': '
-    45
-    ', '
-    ddc-infra-pdu-u7-1.redhorn
-    ': '
-    46
-    '},
-    1623428431: {'ddc-infra-pdu-h9-1.redhorn': '42', 'ddc-infra-pdu-q5-1.redhorn': '40', 'ddc-infra-pdu-s11-1.redhorn': '39', 'ddc-infra-pdu-u11-1.redhorn': '42', 'ddc-infra-pdu-u7-1.redhorn': '44'},
-    1623429031: {'ddc-infra-pdu-h9-1.redhorn': '40', 'ddc-infra-pdu-q5-1.redhorn': '37', 'ddc-infra-pdu-s11-1.redhorn': '37', 'ddc-infra-pdu-u11-1.redhorn': '37', 'ddc-infra-pdu-u7-1.redhorn': '38'},
-    1623429631: {'ddc-infra-pdu-h9-1.redhorn': '38', 'ddc-infra-pdu-q5-1.redhorn': '35', 'ddc-infra-pdu-s11-1.redhorn': '36', 'ddc-infra-pdu-u11-1.redhorn': '36', 'ddc-infra-pdu-u7-1.redhorn': '38'},
-    1623430231: {'ddc-infra-pdu-h9-1.redhorn': '38', 'ddc-infra-pdu-q5-1.redhorn': '37', 'ddc-infra-pdu-s11-1.redhorn': '38', 'ddc-infra-pdu-u11-1.redhorn': '39', 'ddc-infra-pdu-u7-1.redhorn': '38'},
-    1623430831: {'ddc-infra-pdu-h9-1.redhorn': '39', 'ddc-infra-pdu-q5-1.redhorn': '37', 'ddc-infra-pdu-s11-1.redhorn': '37', 'ddc-infra-pdu-u11-1.redhorn': '37', 'ddc-infra-pdu-u7-1.redhorn': '36'},
-    1623431431: {'ddc-infra-pdu-h9-1.redhorn': '40', 'ddc-infra-pdu-q5-1.redhorn': '36', 'ddc-infra-pdu-s11-1.redhorn': '37', 'ddc-infra-pdu-u11-1.redhorn': '35', 'ddc-infra-pdu-u7-1.redhorn': '36'},
-    1623432031: {'ddc-infra-pdu-h9-1.redhorn': '41', 'ddc-infra-pdu-q5-1.redhorn': '37', 'ddc-infra-pdu-s11-1.redhorn': '38', 'ddc-infra-pdu-u11-1.redhorn': '36', 'ddc-infra-pdu-u7-1.redhorn': '37'},
-    1623432631: {'ddc-infra-pdu-h9-1.redhorn': '40', 'ddc-infra-pdu-q5-1.redhorn': '38', 'ddc-infra-pdu-s11-1.redhorn': '39', 'ddc-infra-pdu-u11-1.redhorn': '37', 'ddc-infra-pdu-u7-1.redhorn': '38'},
-    1623433231: {'ddc-infra-pdu-h9-1.redhorn': '39', 'ddc-infra-pdu-q5-1.redhorn': '38', 'ddc-infra-pdu-s11-1.redhorn': '38', 'ddc-infra-pdu-u11-1.redhorn': '38', 'ddc-infra-pdu-u7-1.redhorn': '37'},
-    1623433831: {'ddc-infra-pdu-h9-1.redhorn': '38', 'ddc-infra-pdu-q5-1.redhorn': '39', 'ddc-infra-pdu-s11-1.redhorn': '38', 'ddc-infra-pdu-u11-1.redhorn': '37', 'ddc-infra-pdu-u7-1.redhorn': '37'},
-    1623434431: {'ddc-infra-pdu-h9-1.redhorn': '40', 'ddc-infra-pdu-q5-1.redhorn': '39', 'ddc-infra-pdu-s11-1.redhorn': '39', 'ddc-infra-pdu-u11-1.redhorn': '38', 'ddc-infra-pdu-u7-1.redhorn': '38'},
-    1623435031: {'ddc-infra-pdu-h9-1.redhorn': '41', 'ddc-infra-pdu-q5-1.redhorn': '37', 'ddc-infra-pdu-s11-1.redhorn': '38', 'ddc-infra-pdu-u11-1.redhorn': '36', 'ddc-infra-pdu-u7-1.redhorn': '37'},
-    1623435631: {'ddc-infra-pdu-h9-1.redhorn': '40', 'ddc-infra-pdu-q5-1.redhorn': '37', 'ddc-infra-pdu-s11-1.redhorn': '38', 'ddc-infra-pdu-u11-1.redhorn': '36', 'ddc-infra-pdu-u7-1.redhorn': '36'},
-    1623436231: {'ddc-infra-pdu-h9-1.redhorn': '38', 'ddc-infra-pdu-q5-1.redhorn': '37', 'ddc-infra-pdu-s11-1.redhorn': '38', 'ddc-infra-pdu-u11-1.redhorn': '35', 'ddc-infra-pdu-u7-1.redhorn': '36'},
-    1623436831: {'ddc-infra-pdu-h9-1.redhorn': '37', 'ddc-infra-pdu-q5-1.redhorn': '38', 'ddc-infra-pdu-s11-1.redhorn': '39', 'ddc-infra-pdu-u11-1.redhorn': '36', 'ddc-infra-pdu-u7-1.redhorn': '36'},
-    1623437431: {'ddc-infra-pdu-h9-1.redhorn': '38', 'ddc-infra-pdu-q5-1.redhorn': '37', 'ddc-infra-pdu-s11-1.redhorn': '38', 'ddc-infra-pdu-u11-1.redhorn': '37', 'ddc-infra-pdu-u7-1.redhorn': '37'},
-    1623438031: {'ddc-infra-pdu-h9-1.redhorn': '41', 'ddc-infra-pdu-q5-1.redhorn': '38', 'ddc-infra-pdu-s11-1.redhorn': '39', 'ddc-infra-pdu-u11-1.redhorn': '37', 'ddc-infra-pdu-u7-1.redhorn': '39'},
-    1623438631: {'ddc-infra-pdu-h9-1.redhorn': '40', 'ddc-infra-pdu-q5-1.redhorn': '40', 'ddc-infra-pdu-s11-1.redhorn': '40', 'ddc-infra-pdu-u11-1.redhorn': '38', 'ddc-infra-pdu-u7-1.redhorn': '38'},
-    1623439231: {'ddc-infra-pdu-h9-1.redhorn': '38', 'ddc-infra-pdu-q5-1.redhorn': '37', 'ddc-infra-pdu-s11-1.redhorn': '38', 'ddc-infra-pdu-u11-1.redhorn': '36', 'ddc-infra-pdu-u7-1.redhorn': '35'},
-    1623439831: {'ddc-infra-pdu-h9-1.redhorn': '39', 'ddc-infra-pdu-q5-1.redhorn': '37', 'ddc-infra-pdu-s11-1.redhorn': '38', 'ddc-infra-pdu-u11-1.redhorn': '36', 'ddc-infra-pdu-u7-1.redhorn': '37'},
-    1623440431: {'ddc-infra-pdu-h9-1.redhorn': '39', 'ddc-infra-pdu-q5-1.redhorn': '38', 'ddc-infra-pdu-s11-1.redhorn': '38', 'ddc-infra-pdu-u11-1.redhorn': '38', 'ddc-infra-pdu-u7-1.redhorn': '37'},
-    1623441031: {'ddc-infra-pdu-h9-1.redhorn': '39', 'ddc-infra-pdu-q5-1.redhorn': '39', 'ddc-infra-pdu-s11-1.redhorn': '39', 'ddc-infra-pdu-u11-1.redhorn': '37', 'ddc-infra-pdu-u7-1.redhorn': '37'},
-    1623441631: {'ddc-infra-pdu-h9-1.redhorn': '40', 'ddc-infra-pdu-q5-1.redhorn': '39', 'ddc-infra-pdu-s11-1.redhorn': '40', 'ddc-infra-pdu-u11-1.redhorn': '39', 'ddc-infra-pdu-u7-1.redhorn': '39'},
-    1623442231: {'ddc-infra-pdu-h9-1.redhorn': '40', 'ddc-infra-pdu-q5-1.redhorn': '37', 'ddc-infra-pdu-s11-1.redhorn': '38', 'ddc-infra-pdu-u11-1.redhorn': '36', 'ddc-infra-pdu-u7-1.redhorn': '36'},
-    1623442831: {'ddc-infra-pdu-h9-1.redhorn': '40', 'ddc-infra-pdu-q5-1.redhorn': '38', 'ddc-infra-pdu-s11-1.redhorn': '39', 'ddc-infra-pdu-u11-1.redhorn': '38', 'ddc-infra-pdu-u7-1.redhorn': '39'},
-    1623443431: {'ddc-infra-pdu-h9-1.redhorn': '39', 'ddc-infra-pdu-q5-1.redhorn': '39', 'ddc-infra-pdu-s11-1.redhorn': '39', 'ddc-infra-pdu-u11-1.redhorn': '38', 'ddc-infra-pdu-u7-1.redhorn': '37'},
-    1623444031: {'ddc-infra-pdu-h9-1.redhorn': '38', 'ddc-infra-pdu-q5-1.redhorn': '39', 'ddc-infra-pdu-s11-1.redhorn': '39', 'ddc-infra-pdu-u11-1.redhorn': '39', 'ddc-infra-pdu-u7-1.redhorn': '38'},
-    1623444631: {'ddc-infra-pdu-h9-1.redhorn': '39', 'ddc-infra-pdu-q5-1.redhorn': '37', 'ddc-infra-pdu-s11-1.redhorn': '37', 'ddc-infra-pdu-u11-1.redhorn': '36', 'ddc-infra-pdu-u7-1.redhorn': '36'},
-    1623445231: {'ddc-infra-pdu-h9-1.redhorn': '40', 'ddc-infra-pdu-q5-1.redhorn': '37', 'ddc-infra-pdu-s11-1.redhorn': '37', 'ddc-infra-pdu-u11-1.redhorn': '36', 'ddc-infra-pdu-u7-1.redhorn': '35'},
-    1623445831: {'ddc-infra-pdu-h9-1.redhorn': '40', 'ddc-infra-pdu-q5-1.redhorn': '37', 'ddc-infra-pdu-s11-1.redhorn': '38', 'ddc-infra-pdu-u11-1.redhorn': '35', 'ddc-infra-pdu-u7-1.redhorn': '36'},
-    1623446431: {'ddc-infra-pdu-h9-1.redhorn': '39', 'ddc-infra-pdu-q5-1.redhorn': '37', 'ddc-infra-pdu-s11-1.redhorn': '38', 'ddc-infra-pdu-u11-1.redhorn': '37', 'ddc-infra-pdu-u7-1.redhorn': '37'},
-    1623447031: {'ddc-infra-pdu-h9-1.redhorn': '38', 'ddc-infra-pdu-q5-1.redhorn': '37', 'ddc-infra-pdu-s11-1.redhorn': '39', 'ddc-infra-pdu-u11-1.redhorn': '37', 'ddc-infra-pdu-u7-1.redhorn': '38'},
-    1623447631: {'ddc-infra-pdu-h9-1.redhorn': '38', 'ddc-infra-pdu-q5-1.redhorn': '38', 'ddc-infra-pdu-s11-1.redhorn': '40', 'ddc-infra-pdu-u11-1.redhorn': '37', 'ddc-infra-pdu-u7-1.redhorn': '38'},
-    1623448231: {'ddc-infra-pdu-h9-1.redhorn': '38', 'ddc-infra-pdu-q5-1.redhorn': '39', 'ddc-infra-pdu-s11-1.redhorn': '40', 'ddc-infra-pdu-u11-1.redhorn': '38', 'ddc-infra-pdu-u7-1.redhorn': '40'},
-    1623448831: {'ddc-infra-pdu-h9-1.redhorn': '38', 'ddc-infra-pdu-q5-1.redhorn': '39', 'ddc-infra-pdu-s11-1.redhorn': '39', 'ddc-infra-pdu-u11-1.redhorn': '38', 'ddc-infra-pdu-u7-1.redhorn': '37'},
-    ...
-    }})
-
-*/
--- JSONStaging.h
+++ JSONStaging.h
@@ -1,25 +1,19 @@
-#pragma once
+/*
+ The purpose of this class is to take the data Premetheous is retrieving
+ and compile it in a cleaner fashion for unreal to breakdown.
+*/
+#ifndef JSONSTAGING_H
+#define JSONSTAGING_H
+
 #include <map>
 
-/*
-	The purpose of this class is to take the data Premetheous is retrieving and compile it in a cleaner fashion for unreal to breakdown.
-*/
-
-// MAYBE ADD LATER FOR SLIMMING DOWN DATA INTO SYSTEM FILES INSTEAD OF ONE FILE. 
-//		At this time this isn't needed
-//typedef struct {
-//	std::pair<std::string, float> comp1;
-//	std::pair<std::string, float> comp2;
-//	std::pair<std::string, float> comp3;
-//	std::pair<std::string, float> comp4;
-//	std::pair<std::string, float> comp5;
-//};
-
-int currentTime = 0;
+int currentTime;
 std::vector<std::string> timestampVect;
-std::vector<std::pair<std::string, std::string>> statBySystem;
+std::vector<std::pair<std::string, std::string> > statBySystem;
 std::string holdString;
 
 //Separate the file data into individual lists for later calculation
 int systemCount = 5;
-std::streampos endOfFileRead;
+void createFile();
+std::string constructData(std::string);
+#endif
